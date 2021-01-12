d3b736213287c3a643329969949ff5e093622afe
Ideasurgejava <rajeshkanna.ps@ideasurge.com>
2021-01-12 12:28:38 +0530

    Routing example

---
+++ src/FakeText.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FakeText = () => {
+  return (
+    <div>
+      Contact us pages. Everyone has them, needs them, but are you
+      really paying attention to the strategy behind them? It’s true
+      isn’t it? One of the most valuable pages on your website is
+      usually an afterthought. It’s one of those things that has to be
+      there, but often enough, you throw the necessary information on
+      and leave it at that. What a waste!
+    </div>
+  );
+};
+
+export default FakeText;
